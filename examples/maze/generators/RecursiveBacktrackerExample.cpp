6bf5098e200baac774ac29f2b79410a041270902
Michael Kirkpatrick <mkirkpatrick802@gmail.com>
2023-09-29 12:48:12 -0400

    bro idk

--- examples/maze/generators/RecursiveBacktrackerExample.cpp
+++ examples/maze/generators/RecursiveBacktrackerExample.cpp
@@ -2,8 +2,34 @@
 #include "Random.h"
 #include "RecursiveBacktrackerExample.h"
 #include <climits>
-bool RecursiveBacktrackerExample::Step(World* w) {
 
+bool RecursiveBacktrackerExample::Step(World* w)
+{
+  /*Point2D startingPoint = Point2D(0, 0);
+  stack.push_back(startingPoint);
+  while(!stack.empty())
+  {
+    Point2D currentPoint = stack.front();
+    w->SetNodeColor(currentPoint, Color::Red);
+    std::vector<Point2D> neighbors = getVisitables(w, currentPoint);
+    if(!neighbors.empty())
+    {
+      Point2D targetPoint;
+      if(neighbors.size() == 1)
+      {
+        targetPoint = neighbors.front();
+      }
+      else
+      {
+        targetPoint = neighbors[rand() % neighbors.size()];
+      }
+      stack.push_back(targetPoint);
+    }
+    else
+    {
+      stack.erase(stack.begin());
+    }
+  }*/
 }
 
 void RecursiveBacktrackerExample::Clear(World* world) {
@@ -30,6 +56,29 @@ Point2D RecursiveBacktrackerExample::randomStartPoint(World* world) {
 std::vector<Point2D> RecursiveBacktrackerExample::getVisitables(World* w, const Point2D& p) {
   auto sideOver2 = w->GetSize() / 2;
   std::vector<Point2D> visitables;
+  if(w->GetNorth(p))
+  {
+    Point2D northPoint(p.x, p.y + 1);
+    visitables.push_back(northPoint);
+  }
+
+  if(w->GetEast(p))
+  {
+    Point2D eastPoint(p.x + 1, p.y);
+    visitables.push_back(eastPoint);
+  }
+
+  if(w->GetSouth(p))
+  {
+    Point2D southPoint(p.x, p.y - 1);
+    visitables.push_back(southPoint);
+  }
+
+  if(w->GetWest(p))
+  {
+    Point2D westPoint(p.x - 1, p.y);
+    visitables.push_back(westPoint);
+  }
 
   return visitables;
 }
